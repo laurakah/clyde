2d2dd6f85c938dd2229457b3549cf3144f427735
Luci <mick@d17.de>
2017-07-19 13:39:37 +0200

    SIM: add test for l-map

--- software/sim/gameMapTest.py
+++ software/sim/gameMapTest.py
@@ -20,5 +20,16 @@ class GameMapTestCase(unittest.TestCase):
 		m = gameMap.GameMap("maps/test-room1-box.txt")
 		self.assertEqual(a, m.getMap())
 		
+	def testGameMap_ConvertsTextToArray_LMap(self):
+		a = [[1] * 28]
+		for i in range(0, 2):
+			a.append([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+		a.append([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+		for i in range(0, 2):
+			a.append([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+		a.append([1] * 16)
+		m = gameMap.GameMap("maps/test-room2-l-shape.txt")
+		self.assertEqual(a, m.getMap())
+		
 if __name__ == "__main__":
 	unittest.main()	
