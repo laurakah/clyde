85b626fe2d7688daa17dea73ab99da7a0ed9c6a7
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-07-23 21:41:06 +0200

    SIM: Use tabs instead of spaces (for main.py).

--- software/sim/main.py
+++ software/sim/main.py
@@ -9,61 +9,61 @@ MAPFILE_NAME_STARTSWITH = "test-room"
 INVALID_MAPS = ["test-room0-empty.txt"]
 
 def loadClass(classPath):
-    moduleName = classPath.split(".")[0]
-    className = classPath.split(".")[1]
-    # check module exists. if so, load it
-    if not os.path.exists("%s.py" % moduleName):
-        print "ERROR: module does not exist!"
-        return None
-    moduleObj = __import__(moduleName)
-    try:
-        classObj = getattr(moduleObj, className)
-    except AttributeError:
-        print "ERROR: class does not exist!"
-        return None
-    return classObj
+	moduleName = classPath.split(".")[0]
+	className = classPath.split(".")[1]
+	# check module exists. if so, load it
+	if not os.path.exists("%s.py" % moduleName):
+		print "ERROR: module does not exist!"
+		return None
+	moduleObj = __import__(moduleName)
+	try:
+		classObj = getattr(moduleObj, className)
+	except AttributeError:
+		print "ERROR: class does not exist!"
+		return None
+	return classObj
 
 def launchSim(brainClass, gameMapFile):
-    s = sim.Sim(gameMapFile, brainClass)
-    s.run()
-    rep = s.getReport()
-    return rep
+	s = sim.Sim(gameMapFile, brainClass)
+	s.run()
+	rep = s.getReport()
+	return rep
 
 def launchSimForAllMaps(brainClassPath, mapFileDir, mapFileNameStartsWith, excludeMaps):
-    brainClass = loadClass(brainClassPath)
-    if not brainClass:
-        sys.exit(2)
+	brainClass = loadClass(brainClassPath)
+	if not brainClass:
+		sys.exit(2)
 
-    print "Testing brain \"%s\"" % brainClassPath
-    print "Using maps from directory \"%s\" (excluding %s)" % (mapFileDir, excludeMaps)
+	print "Testing brain \"%s\"" % brainClassPath
+	print "Using maps from directory \"%s\" (excluding %s)" % (mapFileDir, excludeMaps)
 
-    # for each map
+	# for each map
 
-    for entry in os.listdir(mapFileDir):
-        if not entry.startswith(mapFileNameStartsWith):
-            continue
-        if entry in excludeMaps:
-            continue
-        gameMapFile = os.path.join(mapFileDir, entry)
+	for entry in os.listdir(mapFileDir):
+		if not entry.startswith(mapFileNameStartsWith):
+			continue
+		if entry in excludeMaps:
+			continue
+		gameMapFile = os.path.join(mapFileDir, entry)
 
-        # execute simulator
+		# execute simulator
 
-        rep = launchSim(brainClass, gameMapFile)
-        print "Sim executed %d steps on map %s" % (rep['stepCount'], rep['gameMapFile'])
+		rep = launchSim(brainClass, gameMapFile)
+		print "Sim executed %d steps on map %s" % (rep['stepCount'], rep['gameMapFile'])
 
 def main():
-    if len(sys.argv) == 1:
-        print "USAGE: %s <roomDetectionBrain-moduleName.ClassName>" % sys.argv[0]
-        sys.exit(1)
-    brainClassPath = sys.argv[1]
+	if len(sys.argv) == 1:
+		print "USAGE: %s <roomDetectionBrain-moduleName.ClassName>" % sys.argv[0]
+		sys.exit(1)
+	brainClassPath = sys.argv[1]
 
-    mapFileDir              = MAPFILE_DIR
-    mapFileNameStartsWith   = MAPFILE_NAME_STARTSWITH
-    invalidMaps             = INVALID_MAPS
+	mapFileDir		= MAPFILE_DIR
+	mapFileNameStartsWith	= MAPFILE_NAME_STARTSWITH
+	invalidMaps		= INVALID_MAPS
 
-    launchSimForAllMaps(brainClassPath, mapFileDir, mapFileNameStartsWith, invalidMaps)
+	launchSimForAllMaps(brainClassPath, mapFileDir, mapFileNameStartsWith, invalidMaps)
 
-    print "Finished simulation for all maps."
+	print "Finished simulation for all maps."
 
 if __name__ == '__main__':
     main()
