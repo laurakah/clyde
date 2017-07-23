23e74c3b2f5b1826527a3268742254d1103c39ca
Luci <mick@d17.de>
2017-07-23 11:11:47 +0200

    SIM: simulator now gets map from gameMap object

--- software/sim/sim.py
+++ software/sim/sim.py
@@ -1,10 +1,17 @@
+import gameMap
+
 class Sim():
 	
-	def __init__(self):
+	def __init__(self, gameMapFile):
 		self.runningState = False
+		self.gameMapFile = gameMapFile
+		self.gameMap = gameMap.GameMap(self.gameMapFile)
 	
 	def getRunningState(self):
 		return self.runningState
+		
+	def getMap(self):
+		return self.gameMap
 		
 	def start(self):
 		self.runningState = True
--- software/sim/simTest.py
+++ software/sim/simTest.py
@@ -1,11 +1,12 @@
 import unittest
 import sim
+import gameMap
 
 class SimTestCase(unittest.TestCase):
 	
 	def setUp(self):
-# 		self.gameMapFile = "maps/test-room1-box.txt"
-		self.s = sim.Sim()
+		self.gameMapFile = "maps/test-room1-box.txt"
+		self.s = sim.Sim(self.gameMapFile)
 		
 	def tearDown(self):
 		return
@@ -17,6 +18,8 @@ class SimTestCase(unittest.TestCase):
 		self.s.start()
 		self.assertEqual(True, self.s.getRunningState())
 		
+	def testInit_hasMap(self):
+		self.assertIsInstance(self.s.getMap(), gameMap.GameMap)
 	
 	
 if __name__ == "__main__":
