c07376847ff4dd45d2ea5513d821e2bb7947d1b9
Luci <mick@d17.de>
2017-07-23 14:10:27 +0200

    SIM: add time out and run wrapper + tests

--- software/sim/sim.py
+++ software/sim/sim.py
@@ -2,16 +2,47 @@ import gameMap
 
 class Sim():
 	
-	def __init__(self, gameMapFile):
+	DEFAULT_TIMEOUT = 10000
+	
+	def __init__(self, gameMapFile, timeOut = DEFAULT_TIMEOUT):
 		self.runningState = False
+		self.gameOverState = False
 		self.gameMapFile = gameMapFile
 		self.gameMap = gameMap.GameMap(self.gameMapFile)
+		self.stepCount = 0
+		self.timeOut = timeOut
 	
 	def getRunningState(self):
 		return self.runningState
+		
+	def getGameOverState(self):
+		return self.gameOverState
+		
+	def getTimeOut(self):
+		return self.timeOut
+		
+	def setTimeOut(self, timeOut):
+		self.timeOut = timeOut
+		
+	def getStepCount(self):
+		return self.stepCount
 		
 	def getMap(self):
 		return self.gameMap
 		
 	def start(self):
 		self.runningState = True
+		
+	def run(self):
+		self.start()
+		while self.runningState:
+			self.step()
+		
+	def step(self):
+		if self.runningState == False:
+			return
+		if self.stepCount < self.timeOut:
+			self.stepCount += 1
+		else:
+			self.gameOverState = True
+			self.runningState = False
--- software/sim/simTest.py
+++ software/sim/simTest.py
@@ -2,6 +2,13 @@ import unittest
 import sim
 import gameMap
 
+		
+startCalled = False
+
+def fakeStart():
+	global startCalled
+	startCalled = True
+
 class SimTestCase(unittest.TestCase):
 	
 	def setUp(self):
@@ -11,15 +18,70 @@ class SimTestCase(unittest.TestCase):
 	def tearDown(self):
 		return
 		
-	def testInit_runningState_isFalseOnInit(self):
+	def testInit_runningState_isFalse(self):
 		self.assertEqual(False, self.s.getRunningState())
+		
+	def testInit_gameOverState_isFalse(self):
+		self.assertEqual(False, self.s.getGameOverState())
+		
+	def testInit_stepCount_isZero(self):
+		self.assertEqual(0, self.s.getStepCount())
+		
+	def testInit_hasMap(self):
+		self.assertIsInstance(self.s.getMap(), gameMap.GameMap)
+		
+	def testInit_timeOut_isDefault(self):
+		self.assertEqual(sim.Sim.DEFAULT_TIMEOUT, self.s.getTimeOut())
+		
+	def testInit_timeOut_isUserSpecified(self):
+		timeOut = 8888
+		s = sim.Sim(self.gameMapFile, timeOut)
+		self.assertEqual(timeOut, s.getTimeOut())
+		
+	def testSetTimeOut(self):
+		timeOut = 66666
+		self.s.setTimeOut(timeOut)
+		self.assertEqual(timeOut, self.s.getTimeOut())
 		
 	def testStart_setsRunningStateToTrue(self):
 		self.s.start()
 		self.assertEqual(True, self.s.getRunningState())
+	
+	def testRun_callsStart(self):
+		global startCalled
+		startCalled = False
+		self.s.start = fakeStart
+		self.s.run()
+		self.assertEqual(True, startCalled)
 		
-	def testInit_hasMap(self):
-		self.assertIsInstance(self.s.getMap(), gameMap.GameMap)
+	def testRun_runningState_isFalseOnReturn(self):
+		self.s.run()
+		self.assertEqual(False, self.s.getRunningState())
+		
+	def testStep_incrementsStepCount(self):
+		stepCount = self.s.getStepCount()
+		self.s.start()
+		self.s.step()
+		self.assertEqual(stepCount + 1, self.s.getStepCount())
+		
+	def testStep_stepCount_doesNotIncrementWhenRunningStateIsFalse(self):
+		stepCount = self.s.getStepCount()
+		self.s.step()
+		self.assertEqual(stepCount, self.s.getStepCount())
+		
+	def testStep_setsGameOverStateToTrue_onTimeOut(self):
+		self.s.setTimeOut(100)
+		self.s.start()
+		for i in range(0, 101):
+			self.s.step()
+		self.assertEqual(True, self.s.getGameOverState())
+		
+	def testStep_setsRunningStateToFalse_onTimeOut(self):
+		self.s.setTimeOut(100)
+		self.s.start()
+		for i in range(0, 101):
+			self.s.step()
+		self.assertEqual(False, self.s.getRunningState())
 	
 	
 if __name__ == "__main__":
