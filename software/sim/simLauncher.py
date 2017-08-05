ea6f549c850d984f0519792cd8f23763c19e23bc
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-08-05 23:26:12 +0200

    Add simulator launcher.

---
+++ software/sim/simLauncher.py
@@ -0,0 +1,8 @@
+class SimulatorLauncher():
+	@staticmethod
+	def loadClass(classPath):
+		moduleName = classPath.split(".")[0]
+		className = classPath.split(".")[1]
+		moduleObj = __import__(moduleName)
+		classObj = getattr(moduleObj, className)
+		return classObj
---
+++ software/sim/simLauncherTest.py
@@ -0,0 +1,28 @@
+import unittest
+import simLauncher
+import baseRoomDetectionBrain
+
+class SimulatorLauncher(unittest.TestCase):
+
+	def setup(self):
+		return
+
+	def teardown(self):
+		return
+
+	def testLoadClass_loadsClassByClassPath(self):
+		classPath = "baseRoomDetectionBrain.BaseRoomDetectionBrain"
+		classObjExpected = baseRoomDetectionBrain.BaseRoomDetectionBrain
+		classObj = simLauncher.SimulatorLauncher.loadClass(classPath)
+		self.assertEqual(classObjExpected, classObj)
+"""
+	# TODO check None is returned for invalid module
+	def testLoadClass_returnsNoneOnInvalidModule(self):
+		return
+	# TODO check None is returned for invalid class
+	def testLoadClass_returnsNoneOnInvalidClass(self):
+		return
+"""
+
+if __name__ == '__main__':
+	unittest.main()
