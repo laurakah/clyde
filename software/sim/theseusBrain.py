780943991e362a1b4131b35a6c3b25b42b747dd8
Luci <mick@d17.de>
2017-08-12 22:17:09 +0200

    SIM: use symbols for brain orientation

--- software/sim/theseusBrain.py
+++ software/sim/theseusBrain.py
@@ -1,12 +1,13 @@
 import baseBrain
+import player
 
 class TheseusBrain(baseBrain.BaseBrain):
 		
 	def step(self):
 		if self.inputs["isCollision"]():
 			ori = self.inputs["getOrientation"]()
-			if ori == 3:
-				ori = 0
+			if ori == player.Player.ORIENTATION_LEFT:
+				ori = player.Player.ORIENTATION_UP
 			else:
 				ori += 1
 			self.outputs["setOrientation"](ori)
