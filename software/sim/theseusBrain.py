ae3244430295912b0d70ec7e74f3073bfc55a267
Luci <mick@d17.de>
2017-08-12 15:13:00 +0200

    SIM: implement movement functions in step function of brain

--- software/sim/theseusBrain.py
+++ software/sim/theseusBrain.py
@@ -1,9 +1,13 @@
 import baseRoomDetectionBrain
 
 class TheseusBrain(baseRoomDetectionBrain.BaseRoomDetectionBrain):
-
-	def __init__(self, inputs, outputs):
-		pass
 		
-	def isCollision(self):
-		return
+	def step(self):
+		if self.inputs["isCollision"]():
+			ori = self.inputs["getOrientation"]()
+			if ori == 3:
+				ori = 0
+			else:
+				ori += 1
+			self.outputs["setOrientation"](ori)
+		self.outputs["move"]()
--- software/sim/theseusBrainTest.py
+++ software/sim/theseusBrainTest.py
@@ -4,6 +4,11 @@ import baseRoomDetectionBrain
 
 isCollisionCalled = False
 isCollisionValue = False
+getOrientationCalled = False
+getOrientationValue = None
+setOrientationCalled = False
+setOrientationValue = None
+moveCalled = False
 
 def fakeCallback():
 	return
@@ -12,7 +17,24 @@ def fakeIsCollision():
 	global isCollisionCalled
 	global isCollisionValue
 	isCollisionCalled = True
-	isCollisionValue = True
+	return isCollisionValue
+	
+def fakeGetOrientation():
+	global getOrientationCalled
+	global getOrientationValue
+	getOrientationCalled = True
+	return getOrientationValue
+	
+def fakeSetOrientation(ori):
+	global setOrientationCalled
+	global setOrientationValue
+	setOrientationCalled = True
+	setOrientationValue = ori
+	
+def fakeMove():
+	global moveCalled
+	moveCalled = True
+
 
 
 class TheseusBrainTestCase(unittest.TestCase):
@@ -20,7 +42,7 @@ class TheseusBrainTestCase(unittest.TestCase):
 	def setUp(self):
 		self.inputs = {"isCollision": fakeCallback, "getOrientation": fakeCallback, "getMovementDirection": fakeCallback}
 		self.outputs = {"setOrientation": fakeCallback, "setMovementDirection": fakeCallback, "move": fakeCallback}
-		self.b = baseRoomDetectionBrain.BaseRoomDetectionBrain(self.inputs, self.outputs)
+		self.b = theseusBrain.TheseusBrain(self.inputs, self.outputs)
 		
 	def tearDown(self):
 		return
@@ -30,19 +52,104 @@ class TheseusBrainTestCase(unittest.TestCase):
 		
 	def testStep_callsIsCollision(self):
 		global isCollisionCalled
+		global isCollisionValue
 		isCollisionCalled = False
-		b = baseRoomDetectionBrain.BaseRoomDetectionBrain(self.inputs, self.outputs)
-		b.isCollision = fakeIsCollision
-		b.isCollision()
+		isCollisionValue = False
+		self.inputs["isCollision"] = fakeIsCollision
+		b = theseusBrain.TheseusBrain(self.inputs, self.outputs)
+		b.step()
 		self.assertEqual(True, isCollisionCalled)
 		
-	def testStep_returnsIsCollisionValue(self):
+	def testStep_callsGetOrientation_whenIsCollisionIsTrue(self):
 		global isCollisionValue
+		global getOrientationCalled
+		global getOrientationValue
+		isCollisionValue = True
+		getOrientationCalled = False
+		getOrientationValue = 0
+		self.inputs["isCollision"] = fakeIsCollision
+		self.inputs["getOrientation"] = fakeGetOrientation
+		self.outputs["setOrientation"] = fakeSetOrientation
+		b = theseusBrain.TheseusBrain(self.inputs, self.outputs)
+		b.step()
+		self.assertEqual(True, getOrientationCalled)
+		
+	def testStep_doesNotCallGetOrientation_whenIsCollisionIsFalse(self):
+		global isCollisionValue
+		global getOrientationCalled
 		isCollisionValue = False
-		b = baseRoomDetectionBrain.BaseRoomDetectionBrain(self.inputs, self.outputs)
-		b.isCollision = fakeIsCollision
-		b.isCollision()
-		self.assertEqual(True, isCollisionValue)
+		getOrientationCalled = False
+		self.inputs["isCollision"] = fakeIsCollision
+		self.inputs["getOrientation"] = fakeGetOrientation
+		self.outputs["setOrientation"] = fakeSetOrientation
+		b = theseusBrain.TheseusBrain(self.inputs, self.outputs)
+		b.step()
+		self.assertEqual(False, getOrientationCalled)
+		
+	def testStep_callsSetOrientation_whenIsCollisionIsTrue(self):
+		global isCollisionValue
+		global setOrientationCalled
+		global getOrientationValue
+		isCollisionValue = True
+		setOrientationCalled = False
+		getOrientationValue = 0
+		self.inputs["isCollision"] = fakeIsCollision
+		self.inputs["getOrientation"] = fakeGetOrientation
+		self.outputs["setOrientation"] = fakeSetOrientation
+		b = theseusBrain.TheseusBrain(self.inputs, self.outputs)
+		b.step()
+		self.assertEqual(True, setOrientationCalled)
+		
+	def testStep_doesNotCallSetOrientation_whenIsCollisionIsFalse(self):
+		global isCollisionValue
+		global setOrientationCalled
+		isCollisionValue = False
+		setOrientationCalled = False
+		self.inputs["isCollision"] = fakeIsCollision
+		self.inputs["getOrientation"] = fakeGetOrientation
+		self.outputs["setOrientation"] = fakeSetOrientation
+		b = theseusBrain.TheseusBrain(self.inputs, self.outputs)
+		b.step()
+		self.assertEqual(False, setOrientationCalled)
+		
+	def testStep_setOrientation_changesOrientationClockwise(self):
+		global getOrientationValue
+		global setOrientationValue
+		global isCollisionValue
+		isCollisionValue = True
+		getOrientationValue = 0
+		setOrientationValue = None
+		self.inputs["isCollision"] = fakeIsCollision
+		self.inputs["getOrientation"] = fakeGetOrientation
+		self.outputs["setOrientation"] = fakeSetOrientation
+		b = theseusBrain.TheseusBrain(self.inputs, self.outputs)
+		b.step()
+		self.assertEqual(1, setOrientationValue)
+		
+	def testStep_setOrientation_setsZeroWhenOrientationWasThree(self):
+		global getOrientationValue
+		global setOrientationValue
+		global isCollisionValue
+		isCollisionValue = True
+		getOrientationValue = 3
+		setOrientationValue = None
+		self.inputs["isCollision"] = fakeIsCollision
+		self.inputs["getOrientation"] = fakeGetOrientation
+		self.outputs["setOrientation"] = fakeSetOrientation
+		b = theseusBrain.TheseusBrain(self.inputs, self.outputs)
+		b.step()
+		self.assertEqual(0, setOrientationValue)
+		
+	def testStep_callsMove(self):
+		global moveCalled
+		global isCollisionValue
+		moveCalled = False
+		isCollisionValue = False
+		self.inputs["isCollision"] = fakeIsCollision
+		self.outputs["move"] = fakeMove
+		b = theseusBrain.TheseusBrain(self.inputs, self.outputs)
+		b.step()
+		self.assertEqual(True, moveCalled)
 	
 
 if __name__ == "__main__":
