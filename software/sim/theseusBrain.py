f50e3c67425856365a4ca88d0e17ca2edea15532
Luci <mick@d17.de>
2017-08-23 21:27:28 +0200

    SIM: replace orientation value with global brain symbols

--- software/sim/theseusBrain.py
+++ software/sim/theseusBrain.py
@@ -3,6 +3,8 @@ import player
 
 class TheseusBrain(baseBrain.BaseBrain):
 	
+	
+	
 	def __init__(self, inputs, outputs):
 		baseBrain.BaseBrain.__init__(self, inputs, outputs)
 		self.lastOri = None
@@ -18,10 +20,10 @@ class TheseusBrain(baseBrain.BaseBrain):
 		
 	def step(self):
 		ori = self.inputs["getOrientation"]()
-		if ori == 0:
+		if ori == self.ORIENTATION_UP:
 			loc = self._getLocValue(self.inputs)
 			self.mObj.mArr.append([loc])
-		elif ori == 1:
+		elif ori == self.ORIENTATION_RIGHT:
 			loc = self._getLocValue(self.inputs)
 			self.mObj.mArr[0].append(loc)
 		if self.inputs["isCollision"]():
@@ -42,20 +44,20 @@ class TheseusBrain(baseBrain.BaseBrain):
 		if lastOri == None:
 			return None
 		ori = self.inputs["getOrientation"]()
-		if (lastOri == 0 and ori == 3) or (lastOri - 1 == ori):
+		if (lastOri == self.ORIENTATION_UP and ori == self.ORIENTATION_LEFT) or (lastOri - 1 == ori):
 			return 0
-		elif (lastOri == 3 and ori == 0) or (lastOri + 1 == ori):
+		elif (lastOri == self.ORIENTATION_LEFT and ori == self.ORIENTATION_UP) or (lastOri + 1 == ori):
 			return 1
 			
 	def getNextOrientation(self, cw):
 		ori = self.inputs["getOrientation"]()
 		if cw == True:
-			if ori == 3:
+			if ori == self.ORIENTATION_LEFT:
 				return 0
 			else:
 				return ori + 1
 		else:
-			if ori == 0:
+			if ori == self.ORIENTATION_UP:
 				return 3
 			else:
 				return ori - 1
