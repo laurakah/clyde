937c123df9fb1b356d682ed9044d8c50f24b1b79
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-09-03 18:51:01 +0200

    SIM: Handle KeyboardInterrupt in main.py, fixes #22

--- software/sim/main.py
+++ software/sim/main.py
@@ -145,7 +145,11 @@ def main():
 
 		sl = simLauncher.SimulatorLauncher()
 		sl.launchSim = launchSim	# install our non-TDD launchSim() over the currently stubbed one
-		rv = sl.launchSimForAllMaps(brainClassPath, mapFileDir, mapFileNameStartsWith, invalidMaps, timeout, delay, follow, verbose, position, orientation)
+		try:
+			rv = sl.launchSimForAllMaps(brainClassPath, mapFileDir, mapFileNameStartsWith, invalidMaps, timeout, delay, follow, verbose, position, orientation)
+		except KeyboardInterrupt:
+			sys.stdout.write("\nInterrupted by user ... shutting down!")
+			return
 
 	epilogue(verbose)
 
